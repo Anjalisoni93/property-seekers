95bb0a74e8864a1b55eecc8f1fc48e34718961a9
Anjali Soni <anjalipatadiya@gmail.com>
2022-04-26 14:21:42 -0600

    Add PrivateAuth path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Index from './components/Index';
 import Forgotpassword from './components/Forgotpassword';
 import Login from './components/Login';
-import Offer from './components/Offer';
 import Signin from './components/Signin';
 import Navbar from './components/Navbar';
 import User from './components/User';
+import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
 
@@ -15,8 +16,14 @@ function App() {
      <Router>
        <Routes>
          <Route path = '/' element={<Index/>}/>
-         <Route path = '/offers' element={<Offer/>}/>
-         <Route path = '/user' element={<User/>}/>
+
+         
+         <Route path = '/user' element={<PrivateRoute />}>
+          <Route path = '/user' element={<User/>}/>
+         </Route>
+
+
+         <Route path='/user' element={<User />} />
          <Route path = '/signup' element={<Signin/>}/>
          <Route path = '/login' element={<Login/>}/>
          <Route path = '/forgotpassword' element={<Forgotpassword/>}/>
